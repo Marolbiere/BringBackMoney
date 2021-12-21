e62a9a03d52c47bdf98f6baa5e13e75e403b9dd5
Marolbiere <simon.marolleau01@gmail.com>
2021-12-21 17:42:57 +0100

    [new] first test

---
+++ projet.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+struct Player {
+    int life;
+    int key;
+    int position_x;
+    int position_y;
+    int coins;
+    int cabane_coins;
+};
+
+void init_carte(int x, int y) { //fonction d'initialisation de carte
+
+    //affichage 
+}
+
+
+int main() {
+
+    system("cls");
+    printf("Bring Back Money\n\n"); //titre
+    init_carte(7,7);
+    
+    return 0;
+}
