5b89230821078cd65e1a368611f431e157846257
Marolbiere <simon.marolleau01@gmail.com>
2022-01-12 19:44:45 +0100

    [new] remplissage_buissons()

--- init_carte.h
+++ init_carte.h
@@ -26,17 +26,49 @@ void remplissage_obstacle(char carte[SIZE_X][SIZE_Y]){
 
     printw("Nombre d'obstacles : %d\n", nb_obstacle);
 
-    for (int y = 0; y < SIZE_Y; y++)
+    for (int y = nb_random_y; y < SIZE_Y; y++)
     {
         for (int x = 0; x < SIZE_X; x++)
         {
-            if(nb_random_y == y && nb_obstacle != 0) {
+            if(nb_obstacle != 0) {
                 carte[y][x] = 'X';
                 nb_obstacle-=1;
             }
         }
     }
 }
+
+void remplissage_buissons(char carte[SIZE_X][SIZE_Y]){
+    int lg_buisson_x = 0;
+    int lg_buisson_y = 0;
+    int nb_random_y = 0;
+    srand(time(NULL));
+
+    while(lg_buisson_x <= 2) {lg_buisson_x = rand() %6; }
+    while(nb_random_y <= 0) {nb_random_y = rand() %SIZE_X-2; }
+    
+    while(lg_buisson_y <= 2) {lg_buisson_y = rand() %6; }
+    //lg_buisson_y = lg_buisson_x;
+    int lg_buisson_x_init = lg_buisson_x;
+
+    printw("Buisson : %dX%d\n", lg_buisson_x, lg_buisson_y);
+
+    for (int y = nb_random_y; y < SIZE_Y; y++)
+    {
+        lg_buisson_x = lg_buisson_x_init;
+        for (int x = 0; x < SIZE_X; x++)
+        {
+            if(lg_buisson_y !=0 && lg_buisson_x != 0) {
+                carte[y][x] = 'G';
+                lg_buisson_x-=1;
+            }
+        }
+        if(lg_buisson_y !=0) {
+            lg_buisson_y-=1;
+        }
+    }
+}
+
 
 void remplissage_piece(char carte[SIZE_X][SIZE_Y]){
     srand(time(NULL));
@@ -53,7 +85,7 @@ void remplissage_piece(char carte[SIZE_X][SIZE_Y]){
         for (int x = 0; x < SIZE_X; x++)
         {
             nb_random_x = rand() % SIZE_X;
-            if (nb_random_x == x && nb_piece != 0 && carte[y][x] != 'X') {
+            if (nb_random_x == x && nb_piece != 0 && carte[y][x] != 'X' && carte[y][x] != 'G') {
                 carte[y][x] = 'O';
                 nb_piece-=1;
             }
@@ -71,12 +103,12 @@ void placement_joueur_cabane(char carte[SIZE_X][SIZE_Y]) {
             if(x == SIZE_X - 2 && y == SIZE_Y - 1)
                 carte[y][x] = 'J';
         }
-        
     }
 }
 
 void init_carte(char carte[SIZE_X][SIZE_Y]) { //fonction d'initialisation de carte
     remplissage_espace(carte);
+    remplissage_buissons(carte);
     remplissage_obstacle(carte);
     remplissage_piece(carte);
     placement_joueur_cabane(carte);
